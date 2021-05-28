e79150ef16ea4985f53428510df003e4e0c4c464
Artur <drugartur5@gmail.com>
2021-05-28 12:31:29 +0300

    Updates

---
+++ precache-manifest.484ca86f8b9e6d0725fbd50179fa1f3a.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = [
+  {
+    "revision": "8a424b6c4435c868a72c",
+    "url": "/story/static/css/main.07e4bba5.chunk.css"
+  },
+  {
+    "revision": "8a424b6c4435c868a72c",
+    "url": "/story/static/js/main.72c15d80.chunk.js"
+  },
+  {
+    "revision": "d71227836a13ba3116b6",
+    "url": "/story/static/js/runtime~main.cde8c04a.js"
+  },
+  {
+    "revision": "7d379ec6dccbe1571b46",
+    "url": "/story/static/js/2.b48f5268.chunk.js"
+  },
+  {
+    "revision": "0e80de5a57abb38e782bb8806b706578",
+    "url": "/story/static/media/arrowLeft.0e80de5a.png"
+  },
+  {
+    "revision": "39df94aeab903ab730124a43c3090289",
+    "url": "/story/static/media/css.39df94ae.png"
+  },
+  {
+    "revision": "9ce641dd16e42ac2b3edda77ec5b71dc",
+    "url": "/story/static/media/food.9ce641dd.png"
+  },
+  {
+    "revision": "63a633b97d7894331bb294fcd02537dc",
+    "url": "/story/static/media/fortnite.63a633b9.png"
+  },
+  {
+    "revision": "fef702d6be16503c5950a313e70f2817",
+    "url": "/story/static/media/git.fef702d6.png"
+  },
+  {
+    "revision": "69486691fed40ac96cbc59d1f63bdea9",
+    "url": "/story/static/media/hipsta.69486691.png"
+  },
+  {
+    "revision": "107dd63bd51a998db96a74a470f39bd2",
+    "url": "/story/static/media/movie.107dd63b.png"
+  },
+  {
+    "revision": "5faca351937e25cd03f855bff46aef98",
+    "url": "/story/static/media/todo.5faca351.png"
+  },
+  {
+    "revision": "cba0b89d2bf2d96a1ed26edb5849f804",
+    "url": "/story/static/media/react.cba0b89d.png"
+  },
+  {
+    "revision": "1e3669c4deb8f3f980d1e92ad5355ed3",
+    "url": "/story/static/media/door.1e3669c4.jpg"
+  },
+  {
+    "revision": "2e379667d566ee9ea26d8c3d04f197ed",
+    "url": "/story/static/media/bart.2e379667.png"
+  },
+  {
+    "revision": "f220a30788bdc132c519d4c8ec795a26",
+    "url": "/story/static/media/smile.f220a307.jpg"
+  },
+  {
+    "revision": "38902d81d1676f952ad16fcdb0cbf418",
+    "url": "/story/static/media/bootstrap.38902d81.png"
+  },
+  {
+    "revision": "5f18d188359c99196ac61a476f5e84eb",
+    "url": "/story/static/media/AJAX.5f18d188.png"
+  },
+  {
+    "revision": "528bfe4df036d317c6a9e219c8b59f9c",
+    "url": "/story/static/media/redux.528bfe4d.png"
+  },
+  {
+    "revision": "45c4de49811cd0c5b22ce9a75d7afe93",
+    "url": "/story/static/media/dreaming.45c4de49.jpg"
+  },
+  {
+    "revision": "220aac17b9b4c09bd513937a2bd8e1ca",
+    "url": "/story/static/media/never.220aac17.jpg"
+  },
+  {
+    "revision": "f079ba3566d92beb7a86b2c6e5b296b8",
+    "url": "/story/static/media/javascript.f079ba35.jpg"
+  },
+  {
+    "revision": "966dad7c38c9eb3d7dbaf53704b9bffd",
+    "url": "/story/static/media/myLife.966dad7c.jpg"
+  },
+  {
+    "revision": "cb82c75f804bc605ac45f5a167e538bb",
+    "url": "/story/static/media/daughter.cb82c75f.jpg"
+  },
+  {
+    "revision": "d496d48dc5c8012788681ea43d4b769b",
+    "url": "/story/static/media/love.d496d48d.jpg"
+  },
+  {
+    "revision": "2035c885d345bbe047228573cbc8ac2f",
+    "url": "/story/static/media/dj.2035c885.jpg"
+  },
+  {
+    "revision": "259bd693d18a83e6adeeef322250b281",
+    "url": "/story/static/media/business.259bd693.jpg"
+  },
+  {
+    "revision": "a3c8408aa8cfec9afe72fb85f17d3f15",
+    "url": "/story/static/media/code.a3c8408a.jpg"
+  },
+  {
+    "revision": "4090200c9b662dad49806c0044c4fc48",
+    "url": "/story/static/media/home.4090200c.jpg"
+  },
+  {
+    "revision": "d6b38e2b96b6d596cc1c7ad30acffa5b",
+    "url": "/story/static/media/learn.d6b38e2b.jpg"
+  },
+  {
+    "revision": "436ad6dec21f9c65110a3dac89d673de",
+    "url": "/story/static/media/me.436ad6de.jpg"
+  },
+  {
+    "revision": "adc9d2a64dc65214cfdab37ea123dde2",
+    "url": "/story/index.html"
+  }
+];
